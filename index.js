57613eddcd80dc3c81ccc888206bd7e99855f027
Moudy <moudy.elkammash@gmail.com>
2014-05-29 11:29:58 -0400

    use ejs directly

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var path = require('path');
 var express = require('express');
+var ejs = require('ejs');
 var serializeRoutes = require('./serialize-routes');
 
 var app = express();
+var indexHTML = path.join(__dirname, 'views', 'index.ejs');
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 module.exports = function (router) {
   var data = serializeRoutes(router);
 
   app.get('/', function (req, res) {
     data.namespace = req.originalUrl.replace(/\/$/,'');
-    res.render('index', data);
+    ejs.renderFile(indexHTML, data, function (err, html) {
+      res.send(html);
+    });
   });
 
   return app;
